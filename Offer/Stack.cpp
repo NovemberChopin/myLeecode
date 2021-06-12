bbb347dc53af00d006f9bb9bc89c43523c037fd3
jsfantasy <1377378268@qq.com>
2021-06-12 20:59:21 +0800

    add JZ20 包含min函数的栈

--- Offer/Stack.cpp
+++ Offer/Stack.cpp
@@ -8,7 +8,6 @@ private:
     stack<int> stack1;
     stack<int> stack2;
 public:
-
     /**
      * JZ5 用两个栈实现队列
      * 用两个栈来实现一个队列，完成队列的Push和Pop操作。 队列中的元素为int类型。
@@ -28,5 +27,37 @@ public:
         int num = stack2.top();
         stack2.pop();
         return num;
+    }
+};
+
+/**
+ * JZ20 包含min函数的栈
+ * 正常情况下，栈的push，pop操作都为O(1),但是返回最小值，
+ * 需要遍历整个栈，时间复杂度为O(n)，所以这里需要空间换时间的思想。
+ */ 
+class Solution {
+public:
+    // 空间换时间
+    stack<int> sta, sta2;
+    void push(int value) {
+        sta.push(value);
+        if (sta2.empty())
+            sta2.push(value);
+        else {
+            int min = sta2.top();
+            if (value >= min) 
+                sta2.push(min);
+            else sta2.push(value);
+        }
+    }
+    void pop() {
+        sta.pop();
+        sta2.pop();
+    }
+    int top() {
+        return sta.top();
+    }
+    int min() {
+        return sta2.top();
     }
 };
