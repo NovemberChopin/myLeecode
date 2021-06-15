95cb5c449b266717002f9d549aa889586c45e304
jsfantasy <1377378268@qq.com>
2021-06-15 19:42:23 +0800

    add JZ23 二叉搜索树的后序遍历序列

--- Offer/Stack.cpp
+++ Offer/Stack.cpp
@@ -101,3 +101,35 @@ public:
         return st.empty();
     }
 };
+
+
+/**
+ * JZ23 二叉搜索树的后序遍历序列
+ * 描述：输入一个整数数组，判断该数组是不是某二叉搜索树的后序遍历的结果。
+ * 如果是则返回true,否则返回false。假设输入的数组的任意两个数字
+ * 都互不相同。（ps：我们约定空树不是二叉搜素树）
+ * 
+ * BST的后序序列的合法序列是，对于一个序列S，最后一个元素是x （也就是根），
+ * 如果去掉最后一个元素的序列为T，那么T满足：T可以分成两段，前一段（左子树）
+ * 小于x，后一段（右子树）大于x，且这两段（子树）都是合法的后序序列。
+ * 输入：[4,8,6,12,16,14,10]
+ * 输出：true
+ */ 
+class Solution {
+public:
+    bool judge(vector<int>& a, int l, int r){
+        if(l >= r) return true;
+        int i = r;
+        while(i > l && a[i - 1] > a[r]) 
+            --i;
+        for(int j = i - 1; j >= l; --j) 
+            if(a[j] > a[r]) 
+                return false;
+        return judge(a, l, i - 1) && (judge(a, i, r - 1));
+    }
+    bool VerifySquenceOfBST(vector<int> sequence) {
+        if (!sequence.size())
+            return false;
+        return judge(sequence, 0, sequence.size()-1);
+    }
+};
