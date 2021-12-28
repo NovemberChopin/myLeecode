1a6522d66ccd08c5ba8d006fb3bd05ea64fb600d
jinshi <js_smith@163.com>
2021-12-28 11:24:31 +0800

    add JZ35 link deepcopy

---
+++ Offer/Link/JZ35.cpp
@@ -0,0 +1,35 @@
+
+
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+struct RandomListNode {
+    int label;
+    struct RandomListNode *next, *random;
+    RandomListNode(int x) :
+            label(x), next(NULL), random(NULL) {
+    }
+};
+
+class Solution {
+public:
+    RandomListNode* Clone(RandomListNode* pHead) {
+        if (!pHead) return pHead;
+        unordered_map<RandomListNode*, RandomListNode*> mp;
+        RandomListNode* dummy = new RandomListNode(-1);
+        // 后插入创建链表
+        RandomListNode* pre=dummy, *cur=pHead;
+        while (cur) {
+            RandomListNode* clone = new RandomListNode(cur->label);
+            pre->next = clone;
+            mp[cur] = clone;    // 记录映射关系
+            pre = pre->next;
+            cur = cur->next;
+        }
+        for (auto& [key, value] : mp) {    // 遍历哈希表
+            value->random = key->random == NULL ? NULL : mp[key->random];
+        }
+        return dummy->next;
+    }
+};
